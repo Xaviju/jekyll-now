cdaf294d9960c29beced15314deb81904b3ddece
Xaviju <xaviju@gmail.com>
2016-11-13 11:42:38 +0100

    Update RSS feed parser

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,29 @@
 
     //var stats = $.get("http://localhost:8000/api/v1/stats/system");
     var _fetchPosts = function() {
-      const url = "https://medium.com/feed/@xaviju";
-      feednami.load(url).then(feed => {
-        console.log(feed.entries);
-          var source = $("#entry-template").html();
-          var template = Handlebars.compile(source);
-          var html = template(feed.entries);
-          $("#main").append(html);
-        })
+      var url = "https://medium.com/feed/@xaviju";
+      feednami.load(url, function(response){
+        console.log(response.feed);
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        var html = template(response.feed.entries);
+        $("#main").append(html);
+      })
     };
+
+    var url = 'http://daringfireball.net/feeds/articles'
+  feednami.load(url,function(result){
+    if(result.error){
+      console.log(result.error)
+    }
+    else{
+      var entries = result.feed.entries
+      for(var i = 0; i < entries.length; i++){
+        var entry = entries[i]
+        console.log(entry.title)
+      }
+    }
+  })
 
     var init = function() {
 
